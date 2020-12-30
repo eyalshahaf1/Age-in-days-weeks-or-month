f17f8328c63e6e541693440f3abf5a3db8afa7a6
eyalshahaf1 <57352449+eyalshahaf1@users.noreply.github.com>
2020-12-30 10:16:59 +0200

    Update main.py

--- main.py
+++ main.py
@@ -1,8 +1,5 @@
-# 🚨 Don't change the code below 👇
-age = input("What is your current age?  ")
-# 🚨 Don't change the code above 👆
 
-#Write your code below this line 👇
+age = input("What is your current age?  ")
 
 age_as_int = int(age)
 age = 90 - age_as_int
